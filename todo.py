5e1bc7abf700064fbf0a0a531937fd6d6bb07cfc
jnbeach <joseph.n.beach@outlook.com>
2021-12-17 22:57:38 -0600

    This is the second commit with some meaninglesss changes

--- todo.py
+++ todo.py
@@ -1,7 +1,21 @@
-print("Hello")    # Defining main function 
+print("This is one change.")
+print("This is another change.")
+print("This is some more changes.")
+print("Hello")    # Defining main function
 
-def main():         
-    print("hey there") # Using the special variable  # __name__ 
 
-if __name__=="__main__":     
+def main():
+    print("hey there")  # Using the special variable  # __name__
+    print("This is some more changes.")
+
+
+if __name__ == "__main__":
     main()
+
+
+def hello():
+    print("hello")
+
+
+def hello2():
+    print("Bonjour")
